1ddb123b0b6a1614c8d6504d1867e9819d06b440
sonhia19 <sonia.perez@pyxis.com.uy>
2021-04-22 11:48:21 -0300

    init config

---
+++ src/test/java/com/otravo/viajes/ViajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otravo.viajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
